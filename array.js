bad0c1b0a4affa89c88d3007f9cf06e4a62540fb
jwrightnisha <jwrightnisha@gmail.com>
2025-11-09 22:34:28 -0500

    2

---
+++ array.js
@@ -0,0 +1,9 @@
+        const numbers =[1,2,3,1,4];
+
+        console.log(numbers.indexOf(1));
+        console.log(numbers.lastIndexOf(1));
+
+        console.log(numbers.indexOf(1) !==-1);
+        console.log(numbers.includes(1));
+
+        console.log(numbers.indexOf(1,1));        //searches for elements of '1' after the 2nd element in the array
